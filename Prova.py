2af79ddc3285448d7ac1cc21176eb03ae85a5a79
FrancescoPioDiGiorgio <dgfrancesco12@gmail.com>
2023-02-01 15:22:41 +0100

    Aggiunta metodi per suddividere la pagina in nodi

--- Prova.py
+++ Prova.py
@@ -1,2 +1,13 @@
 
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+html = urlopen("file:///C:/Users/dgfra/Downloads/index.html") #collegamneto alla pagina tramite url
+Obj = BeautifulSoup(html.read()) #legge la pagina
+
+for tag in Obj.find_all(True):
+    if tag.name=='head':
+        for val in tag.contents:
+            if val.name!=None:
+                print(val.name) 
+
+
