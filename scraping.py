9ad8bae2f2d80b97ad39b35c232dd84011ca380b
Dom <d.grittani@studenti.uniba.it>
2023-02-02 12:26:28 +0100

    fatto le modifiche

--- scraping.py
+++ scraping.py
@@ -7,15 +7,23 @@ soup = BeautifulSoup(html, features = "html.parser") #legge la pagina
 #attento che la find_all conta come elementi anche i semplici \n che invece non sono tag :c
         
         
-def children(soup, i = 1, j = 0):
+def children(soup, i = 1, j = 0, results = []):
     tags = list(soup.find_all(recursive = False))
     tags = [tag for tag in tags if tag != "\n"]
     k = len(tags)
     if k == 0: return []
+    results += [(id, child, j) for id, child in zip(range(i, i + k), tags)]
+    print(results)
+    id = i
     for child in tags:
-        print("i = {0}, j = {1}, child = \n{2}\n\n".format(i, j, child))
-        children(child, i + k, i)
+        #print("i = {0}, j = {1}, child = \n{2}\n\n".format(i, j, child))
+        #results += [(id, str(child), j)]
+        children(child, i + k, id, results)
         k -= 1
-        
-
-children(soup)
+        id += 1
+    return results
+    
+res = children(soup)
+for el in res:
+    print(el[0], el[2])
+    print()
