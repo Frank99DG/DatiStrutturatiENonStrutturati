90a16b54fcc53853c16af202bda28196bea4ad08
Dom <d.grittani@studenti.uniba.it>
2023-02-02 10:09:25 +0100

    ciaoooo questo è il codice per scorrere l'albero

--- Prova.py
+++ Prova.py
@@ -1,13 +1,21 @@
-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-html = urlopen("file:///C:/Users/dgfra/Downloads/index.html") #collegamneto alla pagina tramite url
-Obj = BeautifulSoup(html.read()) #legge la pagina
-
-for tag in Obj.find_all(True):
-    if tag.name=='head':
-        for val in tag.contents:
-            if val.name!=None:
-                print(val.name) 
+html = urlopen("file:///C:/Users/domi0/Desktop/index.html") #collegamneto alla pagina tramite url
+soup = BeautifulSoup(html, features = "html.parser") #legge la pagina
 
 
+#attento che la find_all conta come elementi anche i semplici \n che invece non sono tag :c
+        
+        
+def children(soup, i = 1, j = 0):
+    tags = list(soup.find_all(recursive = False))
+    tags = [tag for tag in tags if tag != "\n"]
+    k = len(tags)
+    if k == 0: return []
+    for child in tags:
+        print("i = {0}, j = {1}, child = \n{2}\n\n".format(i, j, child))
+        children(child, i + k, i)
+        k -= 1
+        
+
+children(soup)
